688c5143502d5026f098b24f6d9a64103cb65343
Floris <sk8_floris@hotmail.com>
2015-09-14 14:54:33 +0200

    syn1 done, syn2 not finished

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CC=gcc
 
-all: mysh1 mysh2 mysh3
+all: mysh1 mysh2 mysh3 syn1 syn2
 
 mysh1: mysh1.o
 	$(CC) -o mysh1 mysh1.o 
@@ -11,3 +11,9 @@ mysh2: mysh2.o
 
 mysh3: mysh3.o
 	$(CC) -o mysh3 mysh3.o
+
+syn1: syn1.o
+	$(CC) -o syn1 syn1.o
+
+syn2: syn2.o
+	$(CC) -o syn2 syn2.o
---
+++ syn1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <sys/ipc.h>
+
+void display(char *str) {
+    char *tmp;
+    for (tmp = str; *tmp; tmp++) {
+        write(1, tmp, 1);
+        usleep(100);
+    }
+}
+
+int main() {
+    int sem;
+
+    struct sembuf up =   {0, 1, 0};
+    struct sembuf down = {0, -1, 0};
+
+    sem = semget(IPC_PRIVATE, 1, 0600); /* Create semaphore */
+
+    semop(sem, &up, 1); // Start at value 1
+
+    int i;
+    if (fork()) { // parent
+        for (i = 0; i < 10; i++) {
+            semop(sem, &down, 1);
+            display("Hello world\n");
+            semop(sem, &up, 1);
+        }
+        wait(NULL);
+        semctl(sem, 0, IPC_RMID); // Destroy the mutex after child and parent 
+                                  // are both done.
+    }
+    else { // child
+        for (i = 0; i < 10; i++) {
+            semop(sem, &down, 1);
+            display("Bonjour monde\n");
+            semop(sem, &up, 1);
+        }
+    }
+
+    return 0;
+}
